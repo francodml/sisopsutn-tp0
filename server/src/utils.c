81167f76cac1c929e96ada6378a90ab8972330d1
francodml <7284418+francodml@users.noreply.github.com>
2025-04-13 03:34:30 +0000

    accept on server socket

--- server/src/utils.c
+++ server/src/utils.c
@@ -45,6 +45,9 @@ int esperar_cliente(int socket_servidor)
 
 	// Aceptamos un nuevo cliente
 	int socket_cliente;
+
+    socket_cliente = accept(socket_servidor, NULL, NULL);
+
 	log_info(logger, "Se conecto un cliente!");
 
 	return socket_cliente;
